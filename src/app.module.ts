a6d6259cd291784938ee7eaa22705513db19b44f
RockStar666by <artsiom_sauchuk@epam.com>
2022-08-08 13:28:44 +0300

    feat: sequelize many-to-many connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,17 @@ import * as dbConfig from './database/config/config.json';
 import { User } from './users/user.model';
 import { Group } from './groups/group.model';
 import { GroupsModule } from './groups/groups.module';
+import { UserGroup } from './database/relations/user-group.model';
 const env = process.env.NODE_ENV || 'development';
 const config = dbConfig[env];
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({ ...config, models: [User, Group] }),
+    SequelizeModule.forRoot({
+      ...config,
+      models: [User, Group, UserGroup],
+      autoLoadModels: true,
+    }),
     UsersModule,
     GroupsModule,
   ],
--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -6,9 +6,11 @@ import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 import { Group } from './group.model';
 import { GroupsRepository } from './repository/groups.repository';
+import { User } from 'src/users/user.model';
+import { UserGroup } from 'src/database/relations/user-group.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Group])],
+  imports: [SequelizeModule.forFeature([Group, User, UserGroup])],
   controllers: [GroupsController],
   providers: [
     GroupsService,
